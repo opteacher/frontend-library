f5192da1f4767c6bc24ae94217a8210207f92601
opteacher <opteacher@hotmail.com>
2022-12-13 23:54:26 +0800

    更新侧边菜单

--- src/App.vue
+++ src/App.vue
@@ -7,9 +7,18 @@ import {
   UnorderedListOutlined,
   TableOutlined,
   ImportOutlined,
-  ExportOutlined
+  ExportOutlined,
+  SmileOutlined
 } from '@ant-design/icons-vue'
-import { RouterLink, RouterView } from 'vue-router'
+import { ref } from 'vue'
+import { RouterLink, RouterView, useRouter } from 'vue-router'
+
+const selKey = ref('' as string | undefined)
+const router = useRouter()
+
+function onMuItmClick({ key }: { key: string }) {
+  router.push('/' + key)
+}
 </script>
 
 <template>
@@ -22,54 +31,65 @@ import { RouterLink, RouterView } from 'vue-router'
     </a-layout-header>
     <a-layout>
       <a-layout-sider>
-        <a-menu class="h-full border-r-0" theme="dark">
-          <a-menu-item key="1" class="flex items-baseline mt-0">
+        <a-menu
+          class="h-full border-r-0"
+          theme="dark"
+          v-model:selectedKeys="selKey"
+          @click="onMuItmClick"
+        >
+          <a-menu-item key="color-select" class="flex items-baseline mt-0">
             <template #icon>
               <bg-colors-outlined class="text-lg" />
             </template>
-            <RouterLink to="/color-select">调色盘</RouterLink>
+            调色盘
           </a-menu-item>
-          <a-menu-item key="2" class="flex items-baseline">
+          <a-menu-item key="icon-field" class="flex items-baseline">
+            <template #icon>
+              <smile-outlined class="text-lg" />
+            </template>
+            图标库
+          </a-menu-item>
+          <a-menu-item key="upload-file" class="flex items-baseline">
             <template #icon>
               <upload-outlined class="text-lg" />
             </template>
-            <RouterLink to="/upload-file">上传文件/夹</RouterLink>
+            上传文件/夹
           </a-menu-item>
-          <a-menu-item key="3" class="flex items-baseline">
+          <a-menu-item key="ace-editor" class="flex items-baseline">
             <template #icon>
               <code-sandbox-outlined class="text-lg" />
             </template>
-            <RouterLink to="/ace-editor">代码编辑器</RouterLink>
+            代码编辑器
           </a-menu-item>
-          <a-menu-item key="4" class="flex items-baseline">
+          <a-menu-item key="tag-list" class="flex items-baseline">
             <template #icon>
               <tags-outlined class="text-lg" />
             </template>
-            <RouterLink to="/tag-list">标签列表</RouterLink>
+            标签列表
           </a-menu-item>
-          <a-menu-item key="5" class="flex items-baseline">
+          <a-menu-item key="edit-list" class="flex items-baseline">
             <template #icon>
               <unordered-list-outlined class="text-lg" />
             </template>
-            <RouterLink to="/edit-list">可编辑列表</RouterLink>
+            可编辑列表
           </a-menu-item>
-          <a-menu-item key="6" class="flex items-baseline">
+          <a-menu-item key="editable-table" class="flex items-baseline">
             <template #icon>
               <table-outlined class="text-lg" />
             </template>
-            <RouterLink to="/editable-table">可编辑表</RouterLink>
+            可编辑表
           </a-menu-item>
-          <a-menu-item key="7" class="flex items-baseline">
+          <a-menu-item key="batch-import" class="flex items-baseline">
             <template #icon>
               <import-outlined class="text-lg" />
             </template>
-            <RouterLink to="/batch-import">批量导入</RouterLink>
+            批量导入
           </a-menu-item>
-          <a-menu-item key="8" class="flex items-baseline">
+          <a-menu-item key="batch-export" class="flex items-baseline">
             <template #icon>
               <export-outlined class="text-lg" />
             </template>
-            <RouterLink to="/batch-export">批量导出</RouterLink>
+            批量导出
           </a-menu-item>
         </a-menu>
       </a-layout-sider>
