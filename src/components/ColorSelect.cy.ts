e2eed4d75e87dfaa7caa9d2da53ce8485c86efeb
opteacher <opteacher@hotmail.com>
2023-03-14 02:03:27 +0800

    更新测试案例

--- src/components/ColorSelect.cy.ts
+++ src/components/ColorSelect.cy.ts
@@ -3,13 +3,14 @@ import ColorSelect from './ColorSelect.vue'
 
 describe('<ColorSelect />', () => {
   it('renders', () => {
+    const props = {
+      color: '#FF00FF',
+      'onUpdate:color': cy.spy().as('onColorChange')
+    }
     // see: https://test-utils.vuejs.org/guide/
-    cy.mount(ColorSelect, {
-      props: {
-        color: '#FF00FF'
-      }
-    })
+    cy.mount(ColorSelect, { props })
 
+    // 
     cy.get('.color-value .hex input')
       .should('have.value', '#FF00FF')
       .get('.color-value .rgba-r input')
@@ -18,5 +19,14 @@ describe('<ColorSelect />', () => {
       .should('have.value', '0')
       .get('.color-value .rgba-b input')
       .should('have.value', '255')
+
+    cy.get('.saturation-value')
+      .click(Math.random() * 200, Math.random() * 200)
+      .get('@onColorChange')
+      .should('have.been.called', cy.$$('.color-value .hex input').val())
+
+    cy.then(() => { props.color = '#FF0000' })
+      .get('.color-value .hex input')
+      .should('have.value', '#FF0000')
   })
 })
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,21 @@
 import { fileURLToPath, URL } from 'node:url'
-
+import istanbul from 'vite-plugin-istanbul'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    istanbul({
+      include: 'src/*',
+      exclude: ['node_modules', 'tests/', '*.cy.ts'],
+      extension: [ '.js', '.ts', '.vue' ],
+      requireEnv: true,
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
