d7400e45dbe4624ab193403b1bc19ee7d48a3ef2
opteacher <opteacher@hotmail.com>
2025-04-02 23:53:48 +0800

    项目日志输出模块更新中

--- src/components/OptSclPnl.vue
+++ src/components/OptSclPnl.vue
@@ -1,5 +1,15 @@
 <template>
   <div class="flex flex-col space-y-3" :style="{ height }">
+    <OptSclTbox
+      v-if="toolbox && tboxPos === 'top'"
+      :outputing="outputing"
+      v-model:show-time="showTime"
+      v-model:tailf="tailf"
+      @start="startListen"
+      @stop="stopListen"
+      @copy="cpyMsgs"
+      @clear-msgs="() => messages.splice(0, messages.length)"
+    />
     <div class="flex-1 relative">
       <codemirror
         class="absolute"
@@ -12,228 +22,123 @@
           scrollbarStyle: 'native',
           lineWrapping
         }"
-        border
-        :keep-cursor-in-end="ctrler.lockBtm"
+        :border="true"
+        :keep-cursor-in-end="tailf"
       />
     </div>
-    <div v-if="toolbox" class="flex justify-between">
-      <a-space v-if="ctrler.outputing">
-        <PlayCircleTwoTone two-tone-color="#52c41a" />
-        <span>输出中</span>
-        <LoadingOutlined />
-      </a-space>
-      <a-space v-else>
-        <PauseCircleTwoTone two-tone-color="#eb2f96" />
-        <span>停止</span>
-      </a-space>
-      <a-space>
-        <span>显示时间</span>
-        <a-switch size="small" v-model:checked="ctrler.tmVsb" />
-        <span>保持滚动到底</span>
-        <a-switch size="small" v-model:checked="ctrler.lockBtm" />
-        <a-popover title="警告" trigger="click" placement="topRight" v-model:open="ctrler.clrVsb">
-          <template #content>
-            <div class="space-x-2">
-              <InfoCircleOutlined class="text-amber-500" />
-              <span>确定清屏？历史消息无法查看！</span>
-            </div>
-            <a-divider class="my-2" />
-            <div class="space-x-1 text-right">
-              <a-button size="small" type="primary" danger @click="onClrScnCick">确定</a-button>
-              <a-button size="small" @click="() => setProp(ctrler, 'clrVsb', false)">取消</a-button>
-            </div>
-          </template>
-          <a-button size="small">
-            <template #icon><ClearOutlined /></template>
-            清屏
-          </a-button>
-        </a-popover>
-        <a-popover
-          placement="topRight"
-          v-model:open="ctrler.muVsb"
-          :overlayInnerStyle="{ padding: 0 }"
-        >
-          <template #content>
-            <a-menu mode="vertical" theme="light" :items="ctrler.muItms" @click="onCtrlClick" />
-          </template>
-          <a-button type="text" size="small">
-            <template #icon>
-              <UpOutlined v-if="ctrler.muVsb" />
-              <DownOutlined v-else />
-            </template>
-          </a-button>
-        </a-popover>
-      </a-space>
-    </div>
+    <OptSclTbox
+      v-if="toolbox && tboxPos === 'bottom'"
+      :outputing="outputing"
+      v-model:show-time="showTime"
+      v-model:tailf="tailf"
+      @start="startListen"
+      @stop="stopListen"
+      @copy="cpyMsgs"
+      @clear-msgs="() => messages.splice(0, messages.length)"
+    />
   </div>
 </template>
 
 <script setup lang="ts" name="OptSclPnl">
-import { h, reactive, ref, VNode } from 'vue'
-import {
-  ClearOutlined,
-  PlayCircleTwoTone,
-  PauseCircleTwoTone,
-  UpOutlined,
-  DownOutlined,
-  PlayCircleOutlined,
-  PauseCircleOutlined,
-  InfoCircleOutlined,
-  CopyOutlined,
-  LoadingOutlined
-} from '@ant-design/icons-vue'
-import { setProp, until } from '../utils'
-import { message as msgBox } from 'ant-design-vue'
+import { PropType, ref } from 'vue'
+import { until } from '../utils'
 import dayjs, { Dayjs } from 'dayjs'
 import { TinyEmitter } from 'tiny-emitter'
 import Codemirror from 'codemirror-editor-vue3'
 import mqtt from 'mqtt'
+import OptSclTbox from './OptSclTbox.vue'
 
 const props = defineProps({
   url: { type: String, default: '' }, // 使用SSE推送
   topic: { type: String, default: '' }, // 使用MQTT推送
   emitter: { type: TinyEmitter, default: null },
+  tboxPos: { type: String as PropType<'top' | 'bottom'>, default: 'bottom' },
   toolbox: { type: [Boolean, Array], default: true },
   recvMsg: { type: Function, default: (msg: string) => msg },
   lineWrapping: { type: Boolean, default: false },
   lineNumbers: { type: Boolean, default: false },
-  showTime: { type: Boolean, default: false },
   height: { type: String, default: '100%' }
 })
 const emit = defineEmits(['before-start', 'after-end'])
-const message = ref<{ content: string; time: Dayjs }[]>([])
-const ctrler = reactive<
-  {
-    outputing: boolean
-    lockBtm: boolean
-    muVsb: boolean
-    clrVsb: boolean
-    muItms: {
-      key: string
-      icon: () => VNode
-      label: string
-      title: string
-    }[]
-    tmVsb: boolean
-    ess?: EventSource
-    mqttCli?: mqtt.MqttClient
-  } & Record<string, any>
->({
-  outputing: false,
-  lockBtm: true,
-  muVsb: false,
-  clrVsb: false,
-  tmVsb: props.showTime,
-  muItms: [
-    {
-      key: 'start',
-      icon: () => h(PlayCircleOutlined),
-      label: '开始',
-      title: '开始'
-    },
-    {
-      key: 'stop',
-      icon: () => h(PauseCircleOutlined),
-      label: '停止',
-      title: '停止'
-    },
-    {
-      key: 'copy',
-      icon: () => h(CopyOutlined),
-      label: '复制',
-      title: '复制'
-    }
-  ]
-})
+const messages = ref<{ content: string; time: Dayjs }[]>([])
+const outputing = ref(false)
+const showTime = ref(false)
+const tailf = ref(true)
+const mqttCli = ref<mqtt.MqttClient | null>(null)
+const ess = ref<EventSource | null>(null)
 
 if (props.emitter) {
   props.emitter.on('start', startListen)
   props.emitter.on('stop', stopListen)
   props.emitter.on('clean', () => {
-    message.value = []
+    messages.value = []
   })
   props.emitter.on('message', addMessage)
 }
 
-function onClrScnCick() {
-  message.value = []
-  ctrler.clrVsb = false
-}
-async function onCtrlClick({ key }: { key: 'start' | 'stop' | 'copy' }) {
-  switch (key) {
-    case 'start':
-      await startListen()
-      break
-    case 'stop':
-      await stopListen()
-      break
-    case 'copy':
-      navigator.clipboard.writeText(fmtMessage())
-      msgBox.success('复制成功！')
-      break
-  }
-  ctrler.muVsb = false
-}
-async function startListen() {
+function startListen() {
   emit('before-start')
+  addMessage('等待任务开启……')
   if (props.topic) {
-    await until(() => {
-      try {
-        ctrler.mqttCli = mqtt.connect(props.url, {
-          clientId: 'emqx_' + Math.random().toString(16).substring(2, 8),
-          username: 'admin',
-          password: '59524148chenOP',
-          clean: true
-        })
-      } catch(e) {
-        return Promise.resolve(false)
-      }
-      return Promise.resolve(true)
-    })
-    ctrler.mqttCli?.subscribe(props.topic, { qos: 0 })
-    ctrler.outputing = true
-    addMessage('等待任务开启……')
-    ctrler.mqttCli?.on('connect', () => addMessage('开始任务……'))
-    ctrler.mqttCli?.on('error', (e: any) => addMessage('[ERROR]' + JSON.stringify(e)))
-    ctrler.mqttCli?.on('message', async (topic: string, msg: any) => {
+    if (!mqttCli.value) {
+      mqttCli.value = mqtt.connect('ws://127.0.0.1:8083/mqtt', {
+        clientId: 'emqx_' + Math.random().toString(16).substring(2, 8),
+        username: 'admin',
+        password: '59524148chenOP',
+        clean: true
+      })
+      mqttCli.value?.subscribe(props.topic, { qos: 0 })
+    }
+    mqttCli.value?.on('connect', () => addMessage('开始任务……'))
+    mqttCli.value?.on('error', (e: any) => addMessage('[ERROR]' + JSON.stringify(e)))
+    mqttCli.value?.on('message', async (topic: string, msg: any) => {
       if (topic === props.topic) {
         addMessage(msg.toString())
+        if (msg.toString().startsWith('[STOP]')) {
+          await stopListen()
+        }
       }
     })
   } else {
-    ctrler.ess = new EventSource(props.url)
-    ctrler.outputing = true
-    addMessage('等待任务开启……')
-    ctrler.ess.addEventListener('open', () => addMessage('开始任务……'))
-    ctrler.ess.addEventListener('message', e => addMessage(e.data))
+    if (!ess.value) {
+      ess.value = new EventSource(props.url)
+    }
+    ess.value?.addEventListener('open', () => addMessage('开始任务……'))
+    ess.value?.addEventListener('message', e => addMessage(e.data))
     // 服务器端完成任务后可激活stop事件停止输出
-    ctrler.ess.addEventListener('stop', stopListen)
-    ctrler.ess.addEventListener('error', e => addMessage('[ERROR]' + JSON.stringify(e)))
+    ess.value?.addEventListener('stop', stopListen)
+    ess.value?.addEventListener('error', e => addMessage('[ERROR]' + JSON.stringify(e)))
   }
+  outputing.value = true
 }
 async function stopListen() {
   addMessage('停止任务……')
-  if (ctrler.mqttCli?.connected) {
-    await ctrler.mqttCli?.unsubscribeAsync(props.topic)
-    await ctrler.mqttCli?.endAsync()
+  if (mqttCli.value?.connected) {
+    await mqttCli.value?.unsubscribeAsync(props.topic)
+    await mqttCli.value?.endAsync()
   }
-  if (ctrler.ess?.readyState) {
-    ctrler.ess?.close()
+  if (ess.value?.readyState) {
+    ess.value?.close()
   }
-  ctrler.outputing = false
+  outputing.value = false
   emit('after-end')
 }
 function addMessage(content: string) {
   console.log(content)
-  message.value.push({
+  messages.value.push({
     content: props.recvMsg(content),
     time: dayjs().add(8, 'hour')
   })
 }
+function cpyMsgs() {
+  navigator.clipboard.writeText(fmtMessage())
+}
 // 在行消息前放置：[INFO]、[ERROR]、[WARN]、[TITLE]来改变消息样式（改变的同时会去掉这个前缀），默认不做修饰
 function fmtMessage() {
-  return message.value
-    .map(msg => (ctrler.tmVsb ? msg.time.format('HH:mm:ss') + ' - ' : '') + fixMessage(msg.content))
+  return messages.value
+    .map(
+      msg => (showTime.value ? msg.time.format('HH:mm:ss') + ' - ' : '') + fixMessage(msg.content)
+    )
     .join('\n')
 }
 function fixMessage(msg: string) {
---
+++ src/components/OptSclTbox.vue
@@ -0,0 +1,128 @@
+<template>
+  <div class="flex justify-between">
+    <a-space v-if="outputing">
+      <PlayCircleTwoTone two-tone-color="#52c41a" />
+      <span>输出中</span>
+      <LoadingOutlined />
+    </a-space>
+    <a-space v-else>
+      <PauseCircleTwoTone two-tone-color="#eb2f96" />
+      <span>停止</span>
+    </a-space>
+    <a-popover placement="topRight" v-model:open="showMenu" :overlayInnerStyle="{ padding: 0 }">
+      <template #content>
+        <a-menu
+          mode="vertical"
+          theme="light"
+          :items="Object.entries(muItems).map(([key, value]) => ({ key, ...value }))"
+          @click="onCtrlClick"
+        />
+      </template>
+      <a-button type="text" size="small">
+        <template #icon>
+          <UpOutlined v-if="showMenu" />
+          <DownOutlined v-else />
+        </template>
+      </a-button>
+    </a-popover>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { h, reactive, ref } from 'vue'
+import { message } from 'ant-design-vue'
+import {
+  PlayCircleTwoTone,
+  LoadingOutlined,
+  PauseCircleTwoTone,
+  CopyOutlined,
+  PauseCircleOutlined,
+  PlayCircleOutlined,
+  ClearOutlined,
+  UpOutlined,
+  DownOutlined,
+  FieldTimeOutlined,
+  VerticalAlignBottomOutlined
+} from '@ant-design/icons-vue'
+
+const props = defineProps({
+  outputing: { type: Boolean, required: true },
+  showTime: { type: Boolean, default: false },
+  tailf: { type: Boolean, default: true }
+})
+const emit = defineEmits(['clear-msgs', 'start', 'stop', 'copy', 'update:showTime', 'update:tailf'])
+const showMenu = ref(false)
+const muItems = reactive({
+  start: {
+    icon: () => h(PlayCircleOutlined),
+    label: '开始',
+    title: '开始'
+  },
+  stop: {
+    icon: () => h(PauseCircleOutlined),
+    label: '停止',
+    title: '停止'
+  },
+  copy: {
+    icon: () => h(CopyOutlined),
+    label: '复制',
+    title: '复制'
+  },
+  clean: {
+    icon: () => h(ClearOutlined),
+    label: '清屏',
+    title: '清屏'
+  },
+  time: {
+    icon: () => h(FieldTimeOutlined),
+    label: getTimeLbl(),
+    title: getTimeLbl()
+  },
+  tailf: {
+    icon: () => h(VerticalAlignBottomOutlined),
+    label: '保持到底',
+    title: '保持到底'
+  }
+})
+
+async function onCtrlClick({ key }: { key: keyof typeof muItems }) {
+  switch (key) {
+    case 'start':
+      emit('start')
+      break
+    case 'stop':
+      emit('stop')
+      break
+    case 'copy':
+      emit('copy')
+      message.success('复制成功！')
+      break
+    case 'clean':
+      emit('clear-msgs')
+      break
+    case 'time':
+      emit('update:showTime', !props.showTime)
+      muItems['time'].label = getTimeLbl(!props.showTime)
+      break
+    case 'tailf':
+      emit('update:tailf', !props.tailf)
+      muItems['tailf'].label = !props.tailf ? '自由滚动' : '保持到底'
+      break
+  }
+  showMenu.value = false
+}
+function getTimeLbl(showTime?: boolean) {
+  if (typeof showTime !== 'undefined') {
+    return showTime ? '隐藏时间' : '显示时间'
+  } else {
+    return props.showTime ? '隐藏时间' : '显示时间'
+  } 
+}
+function getTailfLbl(tailf?: boolean) {
+  if (typeof tailf !== 'undefined') {
+    return tailf ? '自由滚动' : '保持到底'
+  } else {
+    return props.tailf ? '自由滚动' : '保持到底'
+  }
+}
+</script>
